9a099a558f37db192d55b911d59bf3d7c1c25c22
sahad <sahadwg@gmail.com>
2023-04-12 18:11:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import React, { FC, useState } from 'react';
+
+interface AppProps{
+
+}
+
+const App:FC<AppProps> = ():JSX.Element=> {
+
+
+
+
+  return (
+   <>
+
+   </>
+  )
+}
+
+export default App
